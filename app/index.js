07a6a181f84699dc2152e712ccf7ad6710ebdb7e
Rinel Benjamim <benjamimrinel192@gmail.com>
2024-09-11 22:03:37 +0100

    cadastrar meta

--- app/index.js
+++ app/index.js
@@ -1,4 +1,23 @@
-let {select}  = require("@inquirer/prompts")
+const {select, input}  = require("@inquirer/prompts")
+
+let meta = {
+    value: "Não desistir",
+    checked: false
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async () =>{
+    const meta = await input({message: "Digite a meta: "})
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia.");
+        return
+    }
+
+    metas.push(
+        {value: meta, ckecked: false}
+    )
+}
 
 const start = async () =>{
 
@@ -26,7 +45,9 @@ const start = async () =>{
 
         switch (opcao) {
             case "cadastrar":
-                console.log("vamos cadastrar")
+                
+                await cadastrarMeta()
+                console.log(metas)
                 break;
             case "listar":
                 console.log("vamos listar")
